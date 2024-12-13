a7d5cf6755ec939b370e5bab76a2f15239d40ca6
Dillon-Duncan <mrduncan.business@gmail.com>
2024-12-13 12:56:23 +0200

    add responsive styles for header navigation links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './header.css';
 
 const Header = () => {
     return (
@@ -8,19 +9,19 @@ const Header = () => {
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
                     <Navbar.Brand to="/user/login">BRICS BANKING</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to='/user/login'>User Login</Nav.Link>
-                        <Nav.Link as={Link} to='/user/register'>User Register</Nav.Link>
+                    <Nav className="ml-auto">
+                        <Nav.Link as={Link} to='/user/login' className='nav-link'>User Login</Nav.Link>
+                        <Nav.Link as={Link} to='/user/register' className='nav-link'>User Register</Nav.Link>
 
-                        <Nav.Link as={Link} to='/user/dashboard'>User Dashboard</Nav.Link>
-                        <Nav.Link as={Link} to='/user/dashboard/transaction'>User Transaction</Nav.Link>
-                        <Nav.Link as={Link} to='/user/dashboard/history'>User History</Nav.Link>
+                        <Nav.Link as={Link} to='/user/dashboard' className='nav-link'>User Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to='/user/dashboard/transaction' className='nav-link'>User Transaction</Nav.Link>
+                        <Nav.Link as={Link} to='/user/dashboard/history' className='nav-link'>User History</Nav.Link>
 
-                        <Nav.Link as={Link} to='/admin/login'>Admin Login</Nav.Link>
+                        <Nav.Link as={Link} to='/admin/login' className='nav-link-admin'>Admin Login</Nav.Link>
 
-                        <Nav.Link as={Link} to='/admin/dashboard'>Admin Dashboard</Nav.Link>
-                        <Nav.Link as={Link} to='/admin/dashboard/approve'>Admin Approve</Nav.Link>
-                        <Nav.Link as={Link} to='/admin/dashboard/history'>Admin History</Nav.Link>
+                        <Nav.Link as={Link} to='/admin/dashboard' className='nav-link-admin'>Admin Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to='/admin/dashboard/approve' className='nav-link-admin'>Admin Approve</Nav.Link>
+                        <Nav.Link as={Link} to='/admin/dashboard/history' className='nav-link-admin'>Admin History</Nav.Link>
 
 
 
