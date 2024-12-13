436ef6f8280d7da40a1fd4cc94b6823a792d4b4f
Dillon-Duncan <mrduncan.business@gmail.com>
2024-12-13 11:15:02 +0200

    add react-router-dom and implement routing for user and admin components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import { Route, Routes } from 'react-router-dom';
+
+import UserHistory from './components/user/dashboard/history/userHistory';
+import UserTransaction from './components/user/dashboard/transaction/userTransaction';
+import UserDashboard from './components/user/dashboard/userDashboard';
+import UserLogin from './components/user/login/userLogin';
+import UserRegister from './components/user/register/userRegister';
+
+import AdminApprove from './components/admin/dashboard/aprrove/adminApprove';
+import AdminHistory from './components/admin/dashboard/history/adminHistory';
+import AdminDashboard from './components/admin/dashboard/adminDashboard';
+import AdminLogin from './components/admin/login/adminLogin';
+
+import Header from './components/header/header';
+
+import NoMatch from './components/nomatch/noMatch';
 
 function App() {
   return (
-    <div className="App">
-      <h1>BRICS Banking</h1>
-      <Button>Click</Button>
-    </div>
+    <>
+    <Header />
+    <Routes>
+      
+      <Route path="/user/login" element={<UserLogin />} />
+      <Route path="/user/register" element={<UserRegister />} />
+      
+      <Route path="/user/dashboard" element={<UserDashboard />} />
+      <Route path="/user/dashboard/history" element={<UserHistory />} />
+      <Route path="/user/dashboard/transaction" element={<UserTransaction />} />
+      
+      <Route path="/admin/login" element={<AdminLogin />} />
+      
+      <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      <Route path="/admin/dashboard/approve" element={<AdminApprove />} />
+      <Route path="/admin/dashboard/history" element={<AdminHistory />} />
+      
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+    </>
   );
 }
 
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-const header = () => 
-{
+const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand to="/user/login">BRICS BANKING</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to='/user/login'>User Login</Nav.Link>
+                        <Nav.Link as={Link} to='/user/register'>User Register</Nav.Link>
+
+                        <Nav.Link as={Link} to='/user/dashboard'>User Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to='/user/dashboard/transaction'>User Transaction</Nav.Link>
+                        <Nav.Link as={Link} to='/user/dashboard/history'>User History</Nav.Link>
+
+                        <Nav.Link as={Link} to='/admin/login'>Admin Login</Nav.Link>
+
+                        <Nav.Link as={Link} to='/admin/dashboard'>Admin Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to='/admin/dashboard/approve'>Admin Approve</Nav.Link>
+                        <Nav.Link as={Link} to='/admin/dashboard/history'>Admin History</Nav.Link>
+
+
+
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
     );
 };
-export default header;
 
+export default Header;
